56e62c998e892a26391ecf587d4e0e37000a0ade
Sachin <sachinakshay11@gmail.com>
2024-11-26 23:46:27 +0530

    Update variables.tf

---
+++ modules/internet_gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The ID of the VPC."
+  type        = string
+}
+
+variable "igw_name" {
+  description = "The name of the Internet Gateway."
+  type        = string
+}
